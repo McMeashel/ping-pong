7c7339c2d79a2bb7b315151eeba7742581ec0f00
McMeashel <147833160+McMeashel@users.noreply.github.com>
2023-10-17 17:06:34 +0300

    Update ping_pong.py
    исправление багов, добавление мяча

--- ping_pong.py
+++ ping_pong.py
@@ -18,17 +18,30 @@ class Gamesprite(sprite.Sprite):
         wind.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(Gamesprite):
-    def update(self):
+    def move_2(self):
         keys = key.get_pressed()
-        '''if (keys[K_UP] or keys[K_w]) and self.rect.x > 10:
+        if keys[K_UP]  and self.rect.y > 10:
+            self.rect.y -=self.speed
+        if keys[K_DOWN] and self.rect.y < 340:
             self.rect.y +=self.speed
-        if (keys[K_DOWN] or keys[K_s]) and self.rect.x < 650:
-            self.rect.x -=self.speed'''
 
-   
+    def move_1(self):
+        keys = key.get_pressed()
+        if  keys[K_w] and self.rect.y > 10:
+            self.rect.y -=self.speed
+        if  keys[K_s] and self.rect.y < 340:
+            self.rect.y +=self.speed
+
+class Ball(Gamesprite):
+    def update(self):   
+        keys = key.get_pressed()
+
+
 
 pl_1 = Player('green_line.png', 100, 175, 10, 20, 150)
 pl_2 = Player('blue_line.png', 600, 175, 10, 20, 150)
+
+ball = Ball('ten_ball_1.png', 300, 175, 10, 50, 50)
 
 
 
@@ -40,10 +53,12 @@ while game:
             game = False
     wind.blit(background, (0,0))
 
-    pl_1.update()
+    pl_1.move_1()
     pl_1.reset()
-    pl_2.update()
+    pl_2.move_2()
     pl_2.reset()
+    ball.update()
+    ball.reset()
 
 
     display.update()   
